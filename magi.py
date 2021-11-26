6ef6730665f4cd980282280b9e2c03b78eafc5c2
Morten Borup Petersen <morten_bp@live.dk>
2021-11-26 17:16:58 +0000

    Mere magi

--- magi.py
+++ magi.py
@@ -4,8 +4,7 @@ import json
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-if not os.path.exists('udgangsmappe'):
-    os.makedirs('udgangsmappe')
+OUTDIR = None
 
 @dataclass
 class Opskrift:
@@ -53,7 +52,7 @@ def parseRecipe(path):
 
 
 def createCookbook():
-    with open(os.path.join('udgangsmappe', 'lillelaban.md'), 'w') as f:
+    with open(os.path.join(OUTDIR, 'lillelaban.md'), 'w') as f:
         f.write(f'# Trøffelheltenes opskriftsgrotte\n\n')
 
         path = os.path.join('opskriftsgrotten', "opskrifter")
@@ -61,7 +60,7 @@ def createCookbook():
         for opskrift in os.listdir(path):
             if opskrift.endswith('.json'):
                 opskrift = parseRecipe(os.path.join(path, opskrift))
-                opskrift.skriv_til_md(os.path.join('udgangsmappe', opskrift.filnavn + ".md"))
+                opskrift.skriv_til_md(os.path.join(OUTDIR, opskrift.filnavn + ".md"))
                 opskrifter.append(opskrift)
 
         f.write(f'## Opskrifter:\n')
@@ -69,4 +68,18 @@ def createCookbook():
 
 
 if __name__ == '__main__':
+    # argparser with an optional buildsite argument
+    import argparse
+    parser = argparse.ArgumentParser(description='Create a cookbook from the opskriftsgrotten')
+    parser.add_argument('--buildsite', action='store_true', help='Build the site')
+    args = parser.parse_args()
+
+    if args.buildsite:
+        OUTDIR = 'docs'
+    else:
+        OUTDIR = 'udgangsmappe'
+
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
+
     createCookbook()
